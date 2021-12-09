960a4939b6e27d7e8963e81b6ce01aaa2bfaab10
Matt Koscica <matt.koscica@gmail.com>
2021-12-09 01:48:38 +0100

    new file: rebuild-tmux.sh

---
+++ rebuild-tmux.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+function l() {
+  echo "$*" 1>&2
+}
+
+l "running git pull..."
+git pull
+echo "################################################################################"
+echo "################################################################################"
+
+l "running make clean..."
+make clean
+
+echo "################################################################################"
+echo "################################################################################"
+
+if [[ "$MY_OS" == "Darwin" ]]; then
+  export ENABLE_UTF8PROC="--enable-utf8proc"
+  export CFLAGS=$( pkg-config --cflags-only-I libutf8proc )
+  export LDFLAGS=$( pkg-config --libs-only-L libutf8proc )
+  echo "CFLAGS=$CFLAGS"
+  echo "LDFLAGS=$LDFLAGS"
+  echo "################################################################################"
+  echo "################################################################################"
+fi
+
+l "running autogen.sh..."
+
+if $(./autogen.sh); then
+  l "triggering build pipeline..."
+  ./configure --prefix=$PWD $ENABLE_UTF8PROC && make && make install && cp -va bin/tmux bin/$(bin/tmux -V | sed "s/ /-/g")-$(date +%F).$(date +%s)
+else
+  l "autogen.sh failed, build aborted"
+fi
+
+echo "################################################################################"
+echo "################################################################################"
+
+echo "to link files to their destination:"
+echo "ln -sv $HOME/git_tree/tmux/bin/tmux /usr/local/bin/"
+echo "sudo mkdir /usr/local/share/man/man1 && sudo cp -va $HOME/git_tree/tmux/tmux.1 /usr/local/share/man/man1/"
+
+l "$0 finishing, enjoy your shiny new tmux!"
