a173896cf14fd91cd934036108f2403679a19a13
Matt Koscica <matt.koscica@gmail.com>
2022-02-08 00:18:03 +0100

    init.zsh: lots of features (warn/die/preflight/build_perl/zsh.git)
    - add warn() and die() subs
    - add preflight() sub (dependency checks)
    - add perl section (calls build_perl.sh)
    - add zsh-users/zsh.git repository
    - various rewrites/additions to build helpers

--- init.zsh
+++ init.zsh
@@ -3,7 +3,23 @@
 # set -x
 
 SCRIPT_PATH="${0:A:h}"
+SCRIPT_NAME=$(basename $0)
 # echo "SCRIPT_PATH=$SCRIPT_PATH"
+
+################################################################################
+################################################################################
+
+function warn() {
+  echo "$(tput setaf 227)[$SCRIPT_NAME]$(tput sgr0) $(tput setaf 9)$*$(tput sgr0)" 1>&2
+}
+
+################################################################################
+################################################################################
+
+function die() {
+  warn "$*"
+  exit 1
+}
 
 ################################################################################
 ################################################################################
@@ -39,6 +55,33 @@ function coloured_banner() {
 ################################################################################
 ################################################################################
 
+function preflight() {
+
+  local missing=0
+  local command_path
+
+  for command in bc git; do
+    command_path="$(command -v $command)"
+    if [[ -z "$command_path" || ! -f "$command_path" ]]; then
+      warn "can't find command $command"
+      missing=1
+    fi
+  done
+
+  if [[ $missing == 1 ]]; then
+    warn "pre-requisite/s missing, bailing :("
+    exit 1
+  fi
+}
+
+################################################################################
+################################################################################
+
+preflight
+
+################################################################################
+################################################################################
+
 coloured_banner "initialising vundle..." yellow
 
 test -d ~/.vim/bundle/Vundle.vim || git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
@@ -51,9 +94,16 @@ coloured_banner "creating and populating $HOME/git_tree..." green
 mkdir -p $HOME/git_tree
 cd $HOME/git_tree
 
-for owner_and_repo in atomicstack/{dotfiles,atomicstack_perl} tmux/tmux junegunn/{fzf,fzf.vim} tmux-plugins/tpm Aloxaf/fzf-tab; do
+for owner_and_repo in \
+    atomicstack/{dotfiles,atomicstack_perl} \
+    Aloxaf/fzf-tab\
+    tmux/tmux \
+    junegunn/{fzf,fzf.vim} \
+    tmux-plugins/tpm \
+    zsh-users/zsh \
+; do
   repo=$(basename $owner_and_repo)
-  if [[ ! -d $repo ]]; then echo timeout 3 git clone github.com:$owner_and_repo; fi
+  if [[ ! -d $repo ]]; then timeout 20 git clone github.com:$owner_and_repo; fi
 done
 
 if [[ ! ~/.dotfiles && ~/git_tree/dotfiles ]]; then
@@ -65,27 +115,45 @@ fi
 ################################################################################
 
 coloured_banner "building tmux..." blue
+tmux_build_dir="$HOME/git_tree/tmux"
+test -d "$tmux_build_dir" || die "can't find tmux_build_dir=$tmux_build_dir"
+cd "$tmux_build_dir"
 
-cd $HOME/git_tree/tmux
-echo $SCRIPT_PATH/rebuild-tmux.sh
+test -x "$tmux_build_dir/bin/tmux" || $SCRIPT_PATH/rebuild-tmux.sh
+test -x "$tmux_build_dir/bin/tmux" || die "can't find tmux at $tmux_build_dir/bin/tmux"
 
-echo "run these to install this tmux build into /usr/local:"
-echo -e "\tln -sv $HOME/git_tree/tmux/bin/tmux /usr/local/bin/"
-echo -e "\tsudo mkdir /usr/local/share/man/man1 && sudo ln -sv $HOME/git_tree/tmux/tmux.1 /usr/local/share/man/man1/"
-
-################################################################################
-################################################################################
-
-coloured_banner 'fetching fzf binary...' magenta
-cd $HOME/git_tree/fzf
-echo ./fetch-latest-fzf-binary.pl
-
-echo "run these to install this fzf build into /usr/local:"
-echo -e "\tln -svf $HOME/git_tree/fzf/bin/fzf{,-tmux} /usr/local/bin/";
-echo -e "\tln -svf $HOME/git_tree/fzf/{plugin,shell}/* /usr/local/share/fzf/";
-################################################################################
-################################################################################
+if [[ ! -f "/usr/local/bin/tmux" ]]; then
+  echo "$(tput setaf 10)run these to install this tmux build into /usr/local:$(tput sgr0)"
+  echo -e "\tln -sv $tmux_build_dir/bin/tmux /usr/local/bin/"
+  echo -e "\tsudo mkdir /usr/local/share/man/man1 && sudo ln -sv $tmux_build_dir/tmux.1 /usr/local/share/man/man1/"
+fi
 
 ################################################################################
 ################################################################################
 
+coloured_banner 'building perl...' lightmagenta
+
+if [[ -z "$PERLPATH" && $( ls $HOME | egrep -c 'perl-5[.]??[.]?' ) == 0 ]]; then
+  ~/git_tree/atomicstack_perl/build_perl.sh
+  echo "$(tput setaf 10)run these to install additional requirements:$(tput sgr0)"
+  echo -e "\t~/perl-5.??.?/bin/cpanm Net::GitHub"
+fi
+
+################################################################################
+################################################################################
+
+coloured_banner 'fetching fzf binary...' cyan
+fzf_build_dir="$HOME/git_tree/fzf"
+test -d "$fzf_build_dir" || die "can't find fzf_build_dir=$fzf_build_dir"
+cd "$fzf_build_dir"
+
+test -x "$fzf_build_dir/bin/fzf" || $SCRIPT_PATH/fetch-latest-fzf-binary.pl
+test -x "$fzf_build_dir/bin/fzf" || die "can't find fzf at $fzf_build_dir/bin/fzf"
+
+if [[ ! -f "/usr/local/bin/fzf" ]]; then
+  echo "$(tput setaf 10)run these to install this fzf build into /usr/local:$(tput sgr0)"
+  echo -e "\tln -svf $HOME/git_tree/fzf/bin/fzf{,-tmux} /usr/local/bin/";
+  echo -e "\tln -svf $HOME/git_tree/fzf/{plugin,shell}/* /usr/local/share/fzf/";
+fi
+################################################################################
+################################################################################
